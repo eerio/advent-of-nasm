bfb33b994a77430f17f09d8f25a026116a81c6ac
Pawel Balawender <pbalawender101@gmail.com>
2020-04-17 00:47:01 +0200

    add generic unix constants

---
+++ src/const.asm
@@ -0,0 +1,16 @@
+; syscalls
+%define OPEN 2
+%define READ 0
+%define WRITE 1
+%define EXIT 60
+
+; fs
+%define STDIN 0
+%define STDOUT 1
+
+; I/O
+%define O_RDONLY 0
+
+; exit codes
+%define EXIT_SUCCESS 0
+
